aeca062f839dbb55ec3afac9204600f9efa1028b
Vianka Rivera <viankalrivera@gmail.com>
2020-05-15 16:30:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total, :discount, :price, :items
+  attr_accessor :total, :discount, :price, :items, :last_transaction
 
   def initialize(discount = 0)
     @total = 0
